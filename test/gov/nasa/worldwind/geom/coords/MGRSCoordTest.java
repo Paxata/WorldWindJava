d39df479f27797fa5c903304bf59cead30b89407
Tom Wang <twang@paxata.com>
2016-09-27 12:34:27 -0700

    Slight revision for test

---
+++ test/gov/nasa/worldwind/geom/coords/MGRSCoordTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2016 United States Government as represented by the Administrator of the
+ * National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ */
+
+package gov.nasa.worldwind.geom.coords;
+
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.globes.Earth;
+import junit.framework.TestCase;
+
+public class MGRSCoordTest
+{
+    public static class MGRSCoordConverterTest extends TestCase
+    {
+       public void testPolar() {
+           Angle lat = Angle.fromDegrees(88), lon = Angle.fromDegrees(120);
+
+           MGRSCoord expectedMGRS = MGRSCoord.fromLatLon(lat, lon);
+           MGRSCoord actualMGRS = MGRSCoord.fromString(expectedMGRS.toString(), new Earth());
+
+           // Unfortunately MGRSCoord does not implement equals
+           // Comparison with toString should be sufficient
+           // Also, MGRSCoord.fromString strips out space in between, so strip out the space in between from
+           // expectedMGRS.toString().
+           assertEquals(expectedMGRS.toString().replaceAll(" ", ""), actualMGRS.toString());
+       }
+    }
+}
